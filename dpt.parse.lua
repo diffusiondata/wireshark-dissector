3339f28faee3aef29aff9b09ff8903ec810736ae
Matt Champion <mattunderscorechampion@gmail.com>
2015-10-02 12:10:41 +0100

    Fixed the display of session Ids to use the full byte value.

--- dpt.parse.lua
+++ dpt.parse.lua
@@ -297,10 +297,14 @@ local function parseWS5ConnectionResponse( tvb, client )
 
 	-- Parse Session ID
 	result.sessionId = {}
-	result.sessionId.serverIdentity = tvb( 3, 8 ):int64()
-	result.sessionId.clientIdentity = tvb( 11, 8 ):int64()
+	result.sessionId.serverIdentity = tvb( 3, 8 ):uint64()
+	result.sessionId.clientIdentity = tvb( 11, 8 ):uint64()
 	result.sessionId.range = tvb( 3, 16 )
-	client.clientId = string.format( "%016X-%016X", result.sessionId.serverIdentity:tonumber(), result.sessionId.clientIdentity:tonumber() )
+	client.clientId = string.format(
+		"%s-%s",
+		string.upper( result.sessionId.serverIdentity:tohex() ),
+		string.upper( result.sessionId.clientIdentity:tohex() )
+	)
 
 	-- Parse session token
 	result.sessionTokenRange = tvb( 19, 24 )
