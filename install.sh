2ef5ff07ed4a414b842cb04ad0a3181c2af9c39a
Matt Champion <mattunderscorechampion@gmail.com>
2015-10-06 20:02:45 +0100

    Update the init.lua file on a Mac to load the dissector.

--- install.sh
+++ install.sh
@@ -1,8 +1,46 @@
 #!/bin/sh
 # Install the DPT wireshark dissector
 
-# Clean up existing files and copy new ones
-rm -f ~/.wireshark/dpt.*.lua && cp ./dpt.*.lua ~/.wireshark
+known_init_locations=(/Applications/Wireshark.app/Contents/Resources/share/wireshark/init.lua)
+init_file=""
 
-# TODO: Find and update init.lua script
+# Install the dissector into the user directory
+function install_dissector {
+    # Clean up existing files and copy new ones
+    rm -f ~/.wireshark/dpt.*.lua && cp ./dpt.*.lua ~/.wireshark
 
+    if [ $? -eq 1 ]; then
+        echo "Failed to install the dissector into the users wireshark directory."
+        echo "Manual work needed to complete installation."
+        echo "The lua files in the git repository need to be copied to the users wireshark directory."
+        echo "The init.lua file used by wireshark to setup the Lua environment must load the dpt.lua file from the users wireshark directory."
+    fi
+}
+
+# Locate the init.lua file and set the init_file variable
+function find_init_file {
+    for file in ${known_init_locations[@]}; do
+        if [ -f ${file} ]; then
+            init_file=${file}
+            return
+        fi
+    done
+    echo "Failed to find init.lua file."
+    echo "Manual work needed to complete installation."
+    echo "The init.lua file used by wireshark to setup the Lua environment must load the dpt.lua file from the users wireshark directory."
+    exit 1
+}
+
+# Updates the file indicated by the init_file variable to load the dissector
+function update_init_file {
+    grep dpt.lua ${init_file} > /dev/null
+    if [ $? -eq 1 ]; then
+        echo '\ndofile(USER_DIR.."dpt.lua")\n' >> $1
+    fi
+}
+
+install_dissector
+find_init_file
+update_init_file
+echo "Install complete"
+exit 0
